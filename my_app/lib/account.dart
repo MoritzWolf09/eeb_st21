b2c9a50efb54fb2898ca4f68e8070f4744df348e
Moritz Wolf <moritzwolf@gmx.de>
2021-05-25 17:36:02 +0200

    Formatting logout button

--- my_app/lib/account.dart
+++ my_app/lib/account.dart
@@ -6,7 +6,6 @@ import 'package:slider_button/slider_button.dart';
 import 'package:provider/provider.dart';
 
 class Account extends StatelessWidget {
-
   List _elements = [
     {'name': 'Rennrad Stevens Aspin', 'group': 'Bike'},
     {'name': 'Mountain Bike Stevens S7', 'group': 'Bike'},
@@ -22,80 +21,66 @@ class Account extends StatelessWidget {
       primary: false,
       slivers: <Widget>[
         SliverList(
-            delegate: SliverChildListDelegate(
-              [
-                buildSummary(),
-                buildLogoutButton(context),
-                buildBoldText("Your vehicle list"),
-                buildVehicleList(),
-              ]
-            )
-        )
+            delegate: SliverChildListDelegate([
+          buildSummary(),
+          buildLogoutButton(context),
+          buildBoldText("Your vehicle list"),
+          buildVehicleList(),
+        ]))
       ],
     );
   }
 
   Row buildSummary() {
     return Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          buildAvatarContainer()
-                        ],
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          buildBoldText("6"),
-                          buildText("vehicle(s)")
-                        ],
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          buildBoldText("94"),
-                          buildText("rental(s)")
-                        ],
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          buildBoldText("4,3"),
-                          buildText("rating")
-                        ],
-                      ),
-                    ],
-                  )
-                ],
-              );
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [buildAvatarContainer()],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [buildBoldText("6"), buildText("vehicle(s)")],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [buildBoldText("94"), buildText("rental(s)")],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [buildBoldText("4,3"), buildText("rating")],
+            ),
+          ],
+        )
+      ],
+    );
   }
 
   Container buildAvatarContainer() {
     return Container(
-                padding: const EdgeInsets.all(8),
-                child: Center(
-                    child: CircularProfileAvatar(
-                  null,
-                  child: Icon(
-                    Icons.person,
-                    size: 140,
-                  ),
-                  borderColor: Colors.black,
-                  borderWidth: 3,
-                  elevation: 5,
-                  radius: 75,
-                )),
-              );
+      padding: const EdgeInsets.all(8),
+      child: Center(
+          child: CircularProfileAvatar(
+        null,
+        child: Icon(
+          Icons.person,
+          size: 140,
+        ),
+        borderColor: Colors.black,
+        borderWidth: 3,
+        elevation: 5,
+        radius: 75,
+      )),
+    );
   }
 
   Container buildText(text) {
@@ -115,55 +100,51 @@ class Account extends StatelessWidget {
 
   buildVehicleList() {
     return Container(
-      child: Center(
-        child: GroupedListView<dynamic, String>(
-          scrollDirection: Axis.vertical,
-          shrinkWrap: true,
-          elements: _elements,
-          groupBy: (element) => element['group'],
-          groupComparator: (value1, value2) => value2.compareTo(value1),
-          itemComparator: (item1, item2) =>
-              item1['name'].compareTo(item2['name']),
-          order: GroupedListOrder.DESC,
-          useStickyGroupSeparators: true,
-          groupSeparatorBuilder: (String value) => Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              value,
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
+        child: Center(
+      child: GroupedListView<dynamic, String>(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        elements: _elements,
+        groupBy: (element) => element['group'],
+        groupComparator: (value1, value2) => value2.compareTo(value1),
+        itemComparator: (item1, item2) =>
+            item1['name'].compareTo(item2['name']),
+        order: GroupedListOrder.DESC,
+        useStickyGroupSeparators: true,
+        groupSeparatorBuilder: (String value) => Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            value,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
-          itemBuilder: (c, element) {
-            return Card(
-              elevation: 8.0,
-              margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-              child: Container(
-                child: ListTile(
-                  contentPadding:
-                  EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-                  leading: Icon(Icons.account_circle),
-                  title: Text(element['name']),
-                  trailing: Icon(Icons.arrow_forward),
-                ),
-              ),
-            );
-          },
         ),
-      )
-    );
+        itemBuilder: (c, element) {
+          return Card(
+            elevation: 8.0,
+            margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+            child: Container(
+              child: ListTile(
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                leading: Icon(Icons.account_circle),
+                title: Text(element['name']),
+                trailing: Icon(Icons.arrow_forward),
+              ),
+            ),
+          );
+        },
+      ),
+    ));
   }
 
   buildLogoutButton(BuildContext context) {
     return Container(
       child: Center(
-        child: SliderButton(
-          action: () {
-            context.read<AuthenticationService>().signOut();
-          },
-          label: Text("Slide for logout"),
-          vibrationFlag: false,
-        ),
+        child: ElevatedButton(
+            child: const Text('Logout'),
+            style: ElevatedButton.styleFrom(primary: Colors.redAccent),
+            onPressed: () => {context.read<AuthenticationService>().signOut()}),
       ),
     );
   }
